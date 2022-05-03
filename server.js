11a0296079049cd559441a983f74adc0fabae2cc
mbayopanda <mbayopanda@gmail.com>
2022-05-03 15:54:39 +0100

    initialisation du dossier API CHADRACK

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Bonjour le monde')
+});
+
+app.get('/hello', (req, res, next) => {
+  res.status(200).send('Voici une autre page qui est : Hello')
+});
+
+app.get('/hello/:name', (req, res, next) => {
+  const name = req.params.name;
+  res.status(200).send(`Voici ${name}`);
+});
+
+app.listen(4000, () => {
+  console.log('Server started on port 4000');
+});
