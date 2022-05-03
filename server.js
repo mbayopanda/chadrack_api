2c6c0f859ce5e6bfbcaedf81172340a8b620f613
Chadrack <chadrackmbala20@gmail.com>
2022-05-03 16:44:10 +0100

    Changement

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-  res.status(200).send('Bonjour le monde')
+  res.status(200).send('Bonjour tout le monde')
 });
 
 app.get('/hello', (req, res, next) => {
